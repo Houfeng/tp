83bdf69f309155dcc0b6c904abaab4d80101691d
Houfeng <admin@xhou.net>
2016-08-04 00:16:30 +0800

    dev

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-var path=require("path"),fs=require("fs"),utils=require("real-utils"),CmdLine=require("cmdline"),pkg=require("../package.json"),compile=require("../lib/compile"),cmdLine=new CmdLine,cwd=process.cwd(),src=cmdLine.args[0],dst=cmdLine.args[1];if(cmdLine.options.has("-v"))console.log(pkg.rawName+" "+pkg.version);else if(utils.isNull(src)||utils.isNull(dst))console.log("usage: tp <src> <dst>");else{src=path.resolve(cwd,src),dst=path.resolve(cwd,dst);var funcName=path.basename(dst).split(".")[0],srcBuffer=fs.readFileSync(src).toString(),dstBuffer=compile(funcName,srcBuffer);fs.writeFileSync(dst,dstBuffer)}
+
+var path = require('path');
+var fs = require('fs');
+var utils = require('real-utils');
+var CmdLine = require('cmdline');
+var pkg = require('../package.json');
+var compile = require('../lib/compile');
+var cmdLine = new CmdLine();
+var cwd = process.cwd();
+
+var src = cmdLine.args[0];
+var dst = cmdLine.args[1];
+
+if (cmdLine.options.has('-v')) {
+    console.log(pkg.rawName + ' ' + pkg.version);
+} else if (utils.isNull(src) || utils.isNull(dst)) {
+    console.log('usage: tp <src> <dst>');
+} else {
+    src = path.resolve(cwd, src);
+    dst = path.resolve(cwd, dst);
+    var funcName = path.basename(dst).split('.')[0];
+    var srcBuffer = fs.readFileSync(src).toString();
+    var dstBuffer = compile(funcName, srcBuffer);
+    fs.writeFileSync(dst, dstBuffer);
+}
